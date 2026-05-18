1c2668abcf46ba7b24f05612a127a67196c972ed
Chase Condon <chase.condon@gmail.com>
2026-05-18 14:40:58 +0100

    Clean up workspace: drop stale stardust-site/ cache files; exclude .github-org

--- stardust-site/.astro/content-modules.mjs
+++
@@ -1,7 +0,0 @@
-
-export default new Map([
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/pit/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpit%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/sheets/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsheets%2Findex.mdx&astroContentModuleFlag=true")]]);
-		
